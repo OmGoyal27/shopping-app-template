92eceb722b43f76aa0fe7278615c43502d8cc299
Om Goyal <omgoyal2711@gmail.com>
2025-06-30 08:36:13 +0530

    Removed unnecessary comments

--- app.py
+++ app.py
@@ -22,25 +22,25 @@ def index():
 
 @app.route("/static/<path:filename>")
 def static_files(filename):
-    return send_from_directory("static", filename)      # Just get some basic files, like the favicon
+    return send_from_directory("static", filename)
 
 @app.route("/product/<product_name>")
 def product(product_name):
     product_path = os.path.join(PRODUCTS_DIR, product_name)
-    details_path = os.path.join(product_path, "details.json")  # Get the details of the product
+    details_path = os.path.join(product_path, "details.json")
     if os.path.exists(details_path):  # Only if it exists
         with open(details_path) as f:
             product_details = json.load(f)
         # Mark product as unavailable if stock is 0
         product_details["unavailable"] = product_details.get("stock", 0) == 0
-        return render_template("product.html", product=product_details)  # Serve it.
+        return render_template("product.html", product=product_details)
     else:
         abort(404)
 
 @app.route("/Products/<product_name>/assets/<path:filename>")
 def product_assets(product_name, filename):
     product_assets_path = os.path.join(PRODUCTS_DIR, product_name, "assets")
-    return send_from_directory(product_assets_path, filename)                   # Gets the photos and other assets.
+    return send_from_directory(product_assets_path, filename)
 
 @app.route("/search")
 def search():
@@ -52,10 +52,9 @@ def search():
         if os.path.exists(product_path):
             with open(product_path, "r") as file:
                 product_details = json.load(file)
-                if query in product_details["name"].lower() or query in product_details["description"].lower():         # If the searched query is either in the product name itself, or the description
+                if query in product_details["name"].lower() or query in product_details["description"].lower():
                     matched_products.append(product_details)
 
-    # Render the index.html template with search results
     return render_template("index.html", query=query, products=matched_products, search=True)
 
 @app.route("/cart")
@@ -95,7 +94,7 @@ def add_to_cart(product_name):
             new_quantity = quantity
 
         if new_quantity > available_stock:
-            return redirect(url_for("product", product_name=product_name))  # Redirect back to product page
+            return redirect(url_for("product", product_name=product_name))
 
         cart[product_name] = new_quantity
         session["cart"] = cart
